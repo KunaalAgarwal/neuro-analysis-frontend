2dd32927aa6ecd2914fcf6be4df58a0e79b76173
Kunaal Agarwal <kunaal.agawral1@gmail.com>
2024-10-17 21:16:28 -0400

    Implemented clear workspace

--- src/components/actionsBar.jsx
+++ src/components/actionsBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../styles/actionsBar.css';
 
-const ActionsBar = ({ onNewWorkspace }) => (
+const ActionsBar = ({ onNewWorkspace,  onClearWorkspace}) => (
   <div className="actions-bar">
     <button onClick={onNewWorkspace}>New Workspace</button>
     <div className="separator"></div>
-    <button>Clear Workspace</button>
+    <button onClick={onClearWorkspace}>Clear Workspace</button>
     <div className="separator"></div>
     <button className="btn-primary btn-lg">Generate Workflow</button>
   </div>
