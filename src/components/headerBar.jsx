9b33cb6c213a4036d8e22d3210eaffacc35c5115
Kunaal Agarwal <kunaal.agarwal1@gmail.com>
2025-03-15 17:08:35 -0400

    added some buttons to the header component

--- src/components/headerBar.jsx
+++ src/components/headerBar.jsx
@@ -11,10 +11,16 @@ function HeaderBar(){
     return (
         <div className="header-bar">
             <h1>Neuroimaging Workflow Generator</h1>
-            <span className="info-icon" onClick={handleShowInfo}>ℹ️</span>
+            {/*ℹ️ */}
+            <span className="header-span" onClick={handleShowInfo}>[how-to]</span>
+            <a className="header-span header-link" href="https://github.com/KunaalAgarwal/neuro-analysis-frontend" target="_blank">[github]</a>
+            <a className="header-span header-link" href="https://github.com/KunaalAgarwal/neuro-analysis-frontend/issues" target="_blank">[issues]</a>
             <Modal className="custom-modal" show={showInfo} onHide={handleCloseInfo} centered>
                 <Modal.Body className="modal-label">
-                    Double click nodes to edit parameters; click then press Backspace to remove nodes or edges.
+                    To build a workflow drag and drop nodes from the left-side menu into the canvas.
+                    Double click nodes to edit parameters and draw connections between nodes to prepare your custom workflow.
+                    To delete Click + Backspace. You can also keep your workflows organized using multiple workspaces, all of which
+                    are persistent using in-browser databases.
                 </Modal.Body>
             </Modal>
         </div>
