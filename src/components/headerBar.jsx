ec0eeab80ddbc0b5f64f1c2db8909ec3d24e3c9e
Kunaal Agarwal <kunaal.agarwal1@gmail.com>
2025-03-15 00:27:21 -0400

    added a small info icon

--- src/components/headerBar.jsx
+++ src/components/headerBar.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import '../styles/headerBar.css'; 
+import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
+import '../styles/headerBar.css';
 
 function HeaderBar(){
-  return (
-    <div className="header-bar">
-        <h1>Neuroimaging Workflow Generator</h1>
-    </div>
-  )
+    const [showInfo, setShowInfo] = useState(false);
 
+    const handleShowInfo = () => setShowInfo(true);
+    const handleCloseInfo = () => setShowInfo(false);
+
+    return (
+        <div className="header-bar">
+            <h1>Neuroimaging Workflow Generator</h1>
+            <span className="info-icon" onClick={handleShowInfo}>ℹ️</span>
+            <Modal className="custom-modal" show={showInfo} onHide={handleCloseInfo} centered>
+                <Modal.Body className="modal-label">
+                    Double click nodes to edit parameters; click then press Backspace to remove nodes or edges.
+                </Modal.Body>
+            </Modal>
+        </div>
+    );
 }
 
 export default HeaderBar;
