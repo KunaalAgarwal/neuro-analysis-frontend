ab3ba44a97be24ab6b53e3848d84269c47294ac0
Kunaal Agarwal <kunaal.agarwal1@gmail.com>
2025-11-15 15:01:54 -0500

    moved cwl files into public assets

--- public/cwl/toolMap.js
+++ public/cwl/toolMap.js
@@ -8,6 +8,6 @@ export const TOOL_MAP = {
     'Brain Extraction': {
         id: 'bet',
         cwlPath: 'cwl/fsl/bet.cwl',
-        primaryOutputs: ['brain_image']
+        primaryOutputs: ['brain_extraction']
     }
 };
